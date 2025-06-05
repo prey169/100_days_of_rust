0484053802dca0ea88e6a270857e43e5d57c78ff
prey169 <francisdrossi@gmail.com>
2025-06-05 19:25:56 -0400

    day 1 and 2 done

---
+++ day001_hello_rust/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, rust! Welcome to day 1!");
+}
--- day002_temperature_converter/target/debug/deps/day002_temperature_converter-10cb90946ae6205e.d
+++
@@ -1,9 +0,0 @@
-/home/frossi/Documents/code/100_days_of_rust/day002_temperature_converter/target/debug/deps/libday002_temperature_converter-10cb90946ae6205e.rmeta: src/main.rs Cargo.toml
-
-/home/frossi/Documents/code/100_days_of_rust/day002_temperature_converter/target/debug/deps/day002_temperature_converter-10cb90946ae6205e.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
